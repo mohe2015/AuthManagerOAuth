b2651d9b79baed4c9195c744c0c36befb976b4d4
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-12-25 23:13:57 +0100

    Remove tests

--- tests/phpunit/unit/HooksTest.php
+++ tests/phpunit/unit/HooksTest.php
@@ -9,53 +9,5 @@ use MediaWiki\Extension\AuthManagerOAuth\Hooks;
  */
 class HooksTest extends \MediaWikiUnitTestCase {
 
-	/**
-	 * @covers ::onBeforePageDisplay
-	 */
-	public function testOnBeforePageDisplayVandalizeIsTrue() {
-		$config = new \HashConfig( [
-			'AuthManagerOAuthVandalizeEachPage' => true
-		] );
-		$outputPageMock = $this->getMockBuilder( \OutputPage::class )
-			->disableOriginalConstructor()
-			->getMock();
-		$outputPageMock->method( 'getConfig' )
-			->willReturn( $config );
-
-		$outputPageMock->expects( $this->once() )
-			->method( 'addHTML' )
-			->with( '<p>AuthManagerOAuth was here</p>' );
-		$outputPageMock->expects( $this->once() )
-			->method( 'addModules' )
-			->with( 'oojs-ui-core' );
-
-		$skinMock = $this->getMockBuilder( \Skin::class )
-			->disableOriginalConstructor()
-			->getMock();
-
-		( new Hooks )->onBeforePageDisplay( $outputPageMock, $skinMock );
-	}
-
-	/**
-	 * @covers ::onBeforePageDisplay
-	 */
-	public function testOnBeforePageDisplayVandalizeFalse() {
-		$config = new \HashConfig( [
-			'AuthManagerOAuthVandalizeEachPage' => false
-		] );
-		$outputPageMock = $this->getMockBuilder( \OutputPage::class )
-			->disableOriginalConstructor()
-			->getMock();
-		$outputPageMock->method( 'getConfig' )
-			->willReturn( $config );
-		$outputPageMock->expects( $this->never() )
-			->method( 'addHTML' );
-		$outputPageMock->expects( $this->never() )
-			->method( 'addModules' );
-		$skinMock = $this->getMockBuilder( \Skin::class )
-			->disableOriginalConstructor()
-			->getMock();
-		( new Hooks )->onBeforePageDisplay( $outputPageMock, $skinMock );
-	}
-
+	
 }
